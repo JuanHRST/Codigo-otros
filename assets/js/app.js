83c5752a93cf9f40cf8b14865a4475f3ab8b5dc3
JuanHRST <juanherreras1895@gmail.com>
2023-02-03 18:33:36 -0600

    Codigo-corregido

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,17 @@
-const baseEndpoint = 'https://api.github.com';
+const baseEndpoint = 'https://api.github.com';  
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name'); //Se agrego un .
+const $b = document.querySelector('.blog'); //Se agrego un . en sustitución de #S
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) { //falta la plabra clave async
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  data = await response.json();  //Faltaba esta línea, que sirve para usar la const response
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`; 
+  $b.textContent = `${data.blog}`;   //En las lineas 12,13 y 14, se sustituyó '' con ``
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
